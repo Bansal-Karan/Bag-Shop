b0cfd09fde85044a54451521c473508400bec11a
Bansal-Karan <bansalkaran94651@gmail.com>
2025-01-19 18:02:35 +0530

    Another commit

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const app = express();
-const userModel = require("./models/user");
 const cookieParser = require('cookie-parser');
+const path = require("path");
+const userRouts = require("./routers/userRouts");
+const ownerRouts = require("./routers/ownerRouts");
+const productRouts = require("./routers/productRouts");
+
+const db = require("./config/mongoose-connect");
 
 app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname,"public")));
 
-app.get('/',(req,res) => {
-    res.render("index")
-    console.log("hello");
-    
-})
+app.use("/users",userRouts);
+app.use("/owner",ownerRouts);
+app.use("/product",productRouts);
 
 app.listen(3000)
